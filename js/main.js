59c4d860ccf05e9b9b755da018acc744ab4adf5e
amy-armstrong <a_armstrong148615@fanshaweonline.ca>
2025-12-08 17:47:12 -0500

    Greensock added
    In this commit I added my greensock plugins and did a text animation

--- js/main.js
+++ js/main.js
@@ -1,37 +1,35 @@
 (() => {
-    const menu = document.querySelector("#menu");
-    const hamburgerButton = document.querySelector("#hamburger");
-    const closeButton = document.querySelector("#close");
-  
-    const projects = {
-      project1: "#project-1",
-      project2: "#project-2",
-      project3: "#project-3"
-    };
-    const allProjects = document.querySelectorAll(".project-detail");
-  
-    // my hambuurger
-    function openMenu() {
-      menu.classList.add("open");
-    }
-  
-    function closeMenu() {
-      menu.classList.remove("open");
-    }
-  
-// this is for my projects page
+  const menu = document.querySelector("#menu");
+  const hamburgerButton = document.querySelector("#hamburger");
+  const closeButton = document.querySelector("#close");
 
-//this hides all of my projects
-function hideAllProjects() {
+  const projects = {
+    project1: "#project-1",
+    project2: "#project-2",
+    project3: "#project-3"
+  };
+  const allProjects = document.querySelectorAll(".project-detail");
+
+  // my hamburger
+  function openMenu() {
+    menu.classList.add("open");
+  }
+
+  function closeMenu() {
+    menu.classList.remove("open");
+  }
+
+  // this hides all of my projects
+  function hideAllProjects() {
     for (var i = 0; i < allProjects.length; i++) {
       allProjects[i].style.display = "none";
     }
   }
 
-  // this is showing my project, but one project id at a time depending what is clicked 
+  // show one project id at a time depending what is clicked 
   function showProject(id) {
     if (id && projects[id]) {
-      var p = document.querySelector(projects[id]); //var pointing to the html element
+      var p = document.querySelector(projects[id]);
       if (p) {
         p.style.display = "block";
       }
@@ -39,28 +37,48 @@ function hideAllProjects() {
       console.log("No valid project selected");
     }
   }
-  
-  //grabs the id from the url
+
+  // grabs the id from the url
   function getProjectId() {
-    var query = window.location.search; // "?id=project2"
-    var idStart = query.indexOf("id="); //its where the id starts in url
+    var query = window.location.search; 
+    var idStart = query.indexOf("id="); 
     if (idStart !== -1) {
       var id = "";
-      for (var i = idStart + 3; i < query.length; i++) {//grabs everything after the id=
+      for (var i = idStart + 3; i < query.length; i++) {
         id += query[i];
       }
       return id;
     }
     return null;
   }
+
+  // event listeners
+  hamburgerButton.addEventListener("click", openMenu);
+  closeButton.addEventListener("click", closeMenu);
+
+  // project selection
+  hideAllProjects();
+  showProject(getProjectId());
+
+
   
-  
-    // event Listeners
-    hamburgerButton.addEventListener("click", openMenu);
-    closeButton.addEventListener("click", closeMenu);
-  
-    // project selection
-    hideAllProjects();
-    showProject(getProjectId()); //gets id from url then shows only the porject that matches the id
-  })();
+  gsap.registerPlugin(SplitText, ScrollTrigger);
+
+  // split all elements with class "split"
+  let split = new SplitText(".split", { type: "words, chars" });
+
+  // animate characters on scroll
+  gsap.from(split.chars, {
+    y: 100,
+    autoAlpha: 0,
+    stagger: 0.03,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: ".split",
+      start: "top 80%"
+    }
+  });
+
+})();
+
   
